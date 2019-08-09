a62fc638d15588a9c2f7a88a74d2325c4fda4bf2
zadock <kinara.zadock@gmail.com>
2019-08-09 15:40:21 +0300

    create and construct

---
+++ app/src/main/java/com/kinara/notekeeper/NoteRecyclerAdapter.java
@@ -0,0 +1,46 @@
+package com.kinara.notekeeper;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder>{
+
+    private final Context mContext;
+    private final LayoutInflater mLayoutInflater;
+    private final LayoutInflater mMLayoutInflater;
+
+
+    public NoteRecyclerAdapter(Context context) {
+        mContext = context;
+        mMLayoutInflater = LayoutInflater.from(mContext);
+
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
