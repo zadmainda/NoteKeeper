ccca3a93c5de4aded7b54684ffd25e3635b356ef
zadock <kinara.zadock@gmail.com>
2019-08-14 07:33:40 +0300

    implement next note Bheavior

--- app/src/main/java/com/kinara/notekeeper/NoteActivity.java
+++ app/src/main/java/com/kinara/notekeeper/NoteActivity.java
@@ -153,9 +153,22 @@ public class NoteActivity extends AppCompatActivity {
         } else if (id == R.id.action_cancel) {
             mIsCancelling = true;
             finish();
+        }else if(id == R.id.action_next){
+            moveNext();
+
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void moveNext() {
+        saveNote();
+
+        ++mNotePosition;
+        mNote = DataManager.getInstance().getNotes().get(mNotePosition);
+
+        saveOriginalNoteValues();
+        displayNote(mSpinnerCourses, mTextNoteTitle, mTextNoteText);
     }
 
     private void sendEmail() {
