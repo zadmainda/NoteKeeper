cd3d563bc296f7f5dfad2867f891cef83381fb3f
zadock <kinara.zadock@gmail.com>
2019-08-20 14:30:49 +0300

    make database framework

--- app/src/main/java/com/kinara/notekeeper/NoteKeeperDatabaseContract.java
+++ app/src/main/java/com/kinara/notekeeper/NoteKeeperDatabaseContract.java
@@ -1,10 +1,12 @@
 package com.kinara.notekeeper;
 
-public final class NoteKeeperDatabaseContract {
+import android.provider.BaseColumns;
+
+public final class NoteKeeperDatabaseContract  {
 
     private NoteKeeperDatabaseContract(){}
 
-    public static final class CourseInfoEntry{
+    public static final class CourseInfoEntry implements BaseColumns{
         public static final String TABLE_NAME = "course_info";
         public static final String COLUMN_COURSE_ID = "course_id";
         public static final String COLUMN_COURSE_TITLE = "course_title";
@@ -13,11 +15,14 @@ public final class NoteKeeperDatabaseContract {
 
         public static final String SQL_CREATE_TABLE = "CREATE TABLE " +
                 TABLE_NAME + " (" +
+                _ID + " INTEGER PRIMARY KEY, " +
                 COLUMN_COURSE_ID + " TEXT UNIQUE NOT NULL, " +
                 COLUMN_COURSE_TITLE + " TEXT NOT NULL)";
     }
 
-    public static final class NoteInfoEntry {
+
+
+    public static final class NoteInfoEntry implements BaseColumns {
         public static final String TABLE_NAME = "note_info";
         public static final String COLUMN_NOTE_TITLE = "note_text";
         public static final String COLUMN_NOTE_TEXT = "course_title";
@@ -25,6 +30,7 @@ public final class NoteKeeperDatabaseContract {
 
         public static final String SQL_CREATE_TABLE =
                 "CREATE TABLE " + TABLE_NAME + " (" +
+                        _ID + " INTEGER PRIMARY KEY, " +
                         COLUMN_NOTE_TITLE + " TEXT NOT NULL, " +
                         COLUMN_NOTE_TEXT + " TEXT, " +
                         COLUMN_COURSE_ID + " TEXT NOT NULL)";
