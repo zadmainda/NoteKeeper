16409619c88dff7565db5d43ca672ecd0c41cd83
zadock <kinara.zadock@gmail.com>
2019-08-20 12:15:36 +0300

    acess shared preference(Other stages skipped)

--- app/src/main/java/com/kinara/notekeeper/MainActivity.java
+++ app/src/main/java/com/kinara/notekeeper/MainActivity.java
@@ -1,7 +1,9 @@
 package com.kinara.notekeeper;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.GridLayoutManager;
@@ -17,6 +19,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -58,7 +61,18 @@ public class MainActivity extends AppCompatActivity
     protected void onResume() {
         super.onResume();
         mNoteRecyclerAdapter.notifyDataSetChanged();
+        updateNavHeader();
+    }
 
+    private void updateNavHeader() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+        TextView textUserName = (TextView) headerView.findViewById(R.id.text_user_name);
+        TextView textEmailAdress = (TextView) headerView.findViewById(R.id.text_user_name);
+
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        String username = pref.getString("user_display_name", "");
+        String emailAdress = pref.getString("user_email_adress", "");
     }
 
     private void initializeDisplayContent() {
