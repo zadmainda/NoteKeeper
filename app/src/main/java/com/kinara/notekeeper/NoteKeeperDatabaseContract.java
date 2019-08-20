ca26d6cb43693607853ae1a0ac1746faba7d804b
zadock <kinara.zadock@gmail.com>
2019-08-20 14:09:36 +0300

    implement //add database contract class

---
+++ app/src/main/java/com/kinara/notekeeper/NoteKeeperDatabaseContract.java
@@ -0,0 +1,29 @@
+package com.kinara.notekeeper;
+
+public final class NoteKeeperDatabaseContract {
+
+    private NoteKeeperDatabaseContract(){}
+
+    public static final class CourseInfoEntry{
+        public static final String TABLE_NAME = "course_info";
+        public static final String COLUMN_COURSE_ID = "course_id";
+        public static final String COLUMN_COURSE_TITLE = "course_title";
+
+        //CREATE TABLE course_info (course_id, course_title)
+
+        public static final String SQL_CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " (" +COLUMN_COURSE_ID + ", " + COLUMN_COURSE_TITLE + ")";
+    }
+
+    public static final class NoteInfoEntry {
+        public static final String TABLE_NAME = "note_info";
+        public static final String COLUMN_NOTE_TITLE = "note_text";
+        public static final String COLUMN_NOTE_TEXT = "course_title";
+        public static final String COLUMN_COURSE_ID = "course_id";
+
+        public static final String SQL_CREATE_TABLE =
+                "CREATE TABLE " + TABLE_NAME + " (" +
+                        COLUMN_NOTE_TITLE + ", " +
+                        COLUMN_NOTE_TEXT + ", " +
+                        COLUMN_COURSE_ID + ")";
+    }
+}
